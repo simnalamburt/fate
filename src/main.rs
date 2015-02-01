af56dc697c93e455a612c01c18f1614aca508977
Hyeon Kim <simnalamburt@gmail.com>
2015-02-02 04:05:38 +0900

    Empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(io)]
+
+extern crate glutin;
+extern crate glium;
+
+use std::old_io as io;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::DisplayBuild;
+
+    let _display = glutin::WindowBuilder::new()
+        .with_dimensions(1024, 768)
+        .with_title(format!("Hello world"))
+        .build_glium().unwrap();
+
+    println!("Press ENTER to quit");
+    let _ = io::stdin().read_line();
 }
