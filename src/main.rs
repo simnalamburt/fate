b3da475307ca9b2b7ae3742ef8a6d03339fcbb77
Hyeon Kim <simnalamburt@gmail.com>
2015-02-02 07:24:22 +0900

    Create main loop

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
-#![feature(io, plugin)]
+#![feature(io, std_misc, plugin)]
 
 extern crate glutin;
 #[macro_use] extern crate glium;
 #[plugin] extern crate glium_macros;
 
-use std::old_io as io;
-use glium::{index, Surface};
-
-#[vertex_format]
-#[derive(Copy)]
-struct Vertex {
-    position: [f32; 2],
-    color: [f32; 3],
-}
+use glium::Surface;
 
 fn main() {
     use glium::DisplayBuild;
 
+    // building the display, ie. the main object
     let display = glutin::WindowBuilder::new()
         .with_dimensions(1024, 768)
         .with_title(format!("Hello world"))
         .build_glium().unwrap();
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, vec![
-        Vertex { position: [-0.5, -0.5], color: [0.0, 1.0, 0.0] },
-        Vertex { position: [ 0.0,  0.5], color: [0.0, 0.0, 1.0] },
-        Vertex { position: [ 0.5, -0.5], color: [1.0, 0.0, 0.0] },
-    ]);
+    let vertex_buffer = {
+        #[vertex_format]
+        #[derive(Copy)]
+        struct Vertex {
+            position: [f32; 2],
+            color: [f32; 3],
+        }
 
-    let indices = index::NoIndices(index::PrimitiveType::TrianglesList);
+        glium::VertexBuffer::new(&display, vec![
+            Vertex { position: [-0.5, -0.5], color: [0.0, 1.0, 0.0] },
+            Vertex { position: [ 0.0,  0.5], color: [0.0, 0.0, 1.0] },
+            Vertex { position: [ 0.5, -0.5], color: [1.0, 0.0, 0.0] },
+        ])
+    };
+
+    let index_buffer = glium::IndexBuffer::new(&display, glium::index::TrianglesList(vec![0u16, 1, 2]));
 
     let program = glium::Program::from_source(&display,
         // vertex shader
@@ -69,12 +71,27 @@ fn main() {
         ]
     };
 
-    let mut target = display.draw();
-    target.clear_color(0.0, 0.0, 0.0, 0.0);  // filling the output with the black color
-    target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                &std::default::Default::default()).unwrap();
-    target.finish();
+    // the main loop
+    // each cycle will draw once
+    'main: loop {
+        use std::old_io::timer;
+        use std::time::Duration;
 
-    println!("Press ENTER to quit");
-    let _ = io::stdin().read_line();
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &std::default::Default::default()).unwrap();
+        target.finish();
+
+        // sleeping for some time in order not to use up too much CPU
+        timer::sleep(Duration::milliseconds(17));
+
+        // polling and handling the events received by the window
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => break 'main,
+                _ => ()
+            }
+        }
+    }
 }
