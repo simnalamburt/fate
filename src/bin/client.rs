8ad8f904ad5d85851d17712b276a01d94049be97
Hyeon Kim <simnalamburt@gmail.com>
2015-02-12 05:58:54 +0900

    My own Matrix struct

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -5,6 +5,8 @@ extern crate glutin;
 #[macro_use] extern crate glium;
 extern crate glium_macros;
 
+use math::Matrix;
+
 fn main() {
     use glium::{Surface, DisplayBuild};
 
@@ -68,12 +70,7 @@ fn main() {
     ).unwrap();
 
     let uniforms = uniform! {
-        matrix: [
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
+        matrix: Matrix::one()
     };
 
     let params = glium::DrawParameters {
@@ -102,6 +99,41 @@ fn main() {
                 glutin::Event::Closed => break 'main,
                 _ => ()
             }
+        }
+    }
+}
+
+mod math {
+    use glium::uniforms::*;
+
+    type Pos = (f32, f32, f32);
+    type Dir = (f32, f32, f32);
+
+    pub struct Matrix {
+        m: [f32; 16]
+    }
+
+    impl Matrix {
+        pub fn one() -> Self {
+            Matrix {
+                m: [
+                    1.0, 0.0, 0.0, 1.0,
+                    0.0, 1.0, 0.0, 0.0,
+                    0.0, 0.0, 1.0, 0.0,
+                    0.0, 0.0, 0.0, 1.0,
+                ]
+            }
+        }
+    }
+
+    impl IntoUniformValue<'static> for Matrix {
+        fn into_uniform_value(self) -> UniformValue<'static> {
+            UniformValue::Mat4([
+                [self.m[00], self.m[04], self.m[08], self.m[12]],
+                [self.m[01], self.m[05], self.m[09], self.m[13]],
+                [self.m[02], self.m[06], self.m[10], self.m[14]],
+                [self.m[03], self.m[07], self.m[11], self.m[15]],
+            ])
         }
     }
 }
