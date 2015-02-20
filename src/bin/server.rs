239f93186088a52f850ad6a5f24db7705d8e033c
Seulgi Kim <dev@seulgi.kim>
2015-02-20 14:05:25 +0900

    Use net instead of old_io.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,15 @@
-#![feature(io)]
+#![feature(net)]
 
-use std::old_io::net::udp::UdpSocket;
-use std::old_io::net::ip::{Ipv4Addr, SocketAddr};
+use std::net::UdpSocket;
 
 fn main() {
-    let addr = SocketAddr { ip: Ipv4Addr(127, 0, 0, 1), port: 4567 };
-    let mut socket = match UdpSocket::bind(addr) {
+    let port = 4567;
+    let address = "127.0.0.1";
+    let socket = match UdpSocket::bind(&(address, port)) {
         Ok(s) => s,
         Err(e) => panic!("couldn't bind socket: {}", e),
     };
-    println!("Listening on \x1b[33m{}\x1b[0m", addr);
+    println!("Listening on \x1b[33m{}:{}\x1b[0m", address, port);
 
     let mut buf = [0u8; 1024];
     loop {
@@ -17,7 +17,7 @@ fn main() {
             Ok((amt, src)) => {
                 // Send a reply to the socket we received data from
                 let buf = &buf[..amt];
-                let _ = socket.send_to(buf, src);
+                let _ = socket.send_to(buf, &src);
 
                 let msg = String::from_utf8_lossy(buf);
                 let msg = msg[].trim_right();
