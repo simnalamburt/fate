6a276242fc62ce34ad16e5a6a76cfcb46118f99b
Hyeon Kim <simnalamburt@gmail.com>
2015-07-01 17:06:39 +0900

    Add Nemo

--- client/src/main.rs
+++ client/src/main.rs
@@ -28,6 +28,46 @@ fn main() {
 
 
     //
+    // Nemo
+    //
+    let vb_nemo = glium::VertexBuffer::new(&display, {
+        #[derive(Clone, Copy)]
+        struct Vertex { position: [f32; 2] }
+
+        implement_vertex!(Vertex, position);
+
+        vec![
+            Vertex { position: [ -10.0, -10.0 ] },
+            Vertex { position: [ -10.0,  10.0 ] },
+            Vertex { position: [  10.0, -10.0 ] },
+            Vertex { position: [  10.0,  10.0 ] },
+        ]
+    });
+    let ib_nemo = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+    let program_nemo = glium::Program::from_source(&display,
+        r#"
+            #version 410
+            uniform vec2 cursor;
+            uniform mat4 matrix;
+            in vec2 position;
+
+            void main() {
+                gl_Position = matrix * vec4(position + cursor, 0.0, 1.0);
+            }
+        "#, r#"
+            #version 410
+            out vec3 color;
+
+            void main() {
+                color = vec3(1.0, 0.82745, 0.14118);
+            }
+        "#, None).unwrap();
+    let position = (300.0, 300.0);
+
+    let matrix_game = math::Matrix::orthographic_off_center(0.0, width, 0.0, height, 0.0, 1.0);
+
+
+    //
     // Parameters for UI
     //
     let vb_ui = glium::VertexBuffer::new(&display, {
@@ -73,6 +113,11 @@ fn main() {
     'main: loop {
         use glium::Surface;
 
+        let uniforms_nemo = uniform! {
+            position: position,
+            matrix: matrix_game.clone()
+        };
+
         let uniforms_ui = uniform! {
             cursor: cursor,
             matrix: matrix_ui.clone()
@@ -80,6 +125,7 @@ fn main() {
 
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
+        target.draw(&vb_nemo, &ib_nemo, &program_nemo, &uniforms_nemo, &Default::default()).unwrap();
         target.draw(&vb_ui, &ib_ui, &program_ui, &uniforms_ui, &Default::default()).unwrap();
         let _ = target.finish();
 
