984db791f61f08666d0133c99d28e18d2cc37940
Hyeon Kim <simnalamburt@gmail.com>
2015-07-01 20:45:35 +0900

    Now Nemo moves with animation
    Implemented finite state machine.
    
    Note:
    
        let mut next = None;
    
        match self.state {
            State::Stopped { .. } => {}
            State::Moving { dest, ref mut t, .. } => {
                *t += elapsed / 0.5;
                if 1.0 <= *t { next = Some(State::Stopped { pos: dest }); }
            }
        };
    
        next.map(|next| {
            self.state = next;
        });
    
    `State::Moving { dest, ref mut t, .. }` 이 문장에서 t가 ref mut이어서,
    t는 self.state 변수의 소유권을 가져간다. 그래서 t 변수가 살아있는한
    self.state는 직접 수정할 수 없게 되기 때문에, next라는 변수를 별도로
    만들어서 t 변수의 수명이 끝났을 때 self.state를 수정한다.

--- client/src/main.rs
+++ client/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     //
     let (width, height) = {
         let dim = display.get_framebuffer_dimensions();
-        (dim.0 as f32, dim.1 as f32)
+        (dim.0 as f64, dim.1 as f64)
     };
 
 
@@ -30,7 +30,7 @@ fn main() {
     // Game
     //
     let mut nemo = nemo::Nemo::new(&display);
-    let world = math::Matrix::orthographic(width/10.0, height/10.0, 0.0, 1.0);
+    let world = math::Matrix::orthographic(width as f32/10.0, height as f32/10.0, 0.0, 1.0);
 
 
     //
@@ -69,7 +69,7 @@ fn main() {
             }
         "#, None).unwrap();
     let mut cursor = (300.0, 300.0);
-    let matrix_ui = math::Matrix::orthographic_off_center(0.0, width, 0.0, height, 0.0, 1.0);
+    let matrix_ui = math::Matrix::orthographic_off_center(0.0, width as f32, 0.0, height as f32, 0.0, 1.0);
 
 
     let mut last = PreciseTime::now();
@@ -79,25 +79,16 @@ fn main() {
     'main: loop {
         use glium::Surface;
 
-        let uniforms_ui = uniform! {
-            cursor: cursor,
-            matrix: matrix_ui.clone()
-        };
-
-        let mut target = display.draw();
-        target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
-        let mut target = nemo.draw(target, world.clone()); // ㅇㅅㅇ?
-        target.draw(&vb_ui, &ib_ui, &program_ui, &uniforms_ui, &Default::default()).unwrap();
-        let _ = target.finish();
-
-        // polling and handling the events received by the window
+        //
+        // Poll and handle the events received by the window
+        //
         for event in display.poll_events() {
             use glium::glutin::Event::*;
             use glium::glutin::ElementState::*;
             use glium::glutin::MouseButton::*;
 
             match event {
-                MouseMoved((x, y)) => cursor = (x as f32, height - y as f32),
+                MouseMoved((x, y)) => cursor = (x as f64, height - y as f64),
                 MouseInput(Pressed, Left) => nemo.go({
                     // 마우스 좌표계 ~ 게임 좌표계 변환
                     ((cursor.0 - width/2.0)/10.0, (cursor.1 - height/2.0)/10.0)
@@ -107,8 +98,30 @@ fn main() {
             }
         }
 
+
+        //
+        // Update
+        //
         let now = PreciseTime::now();
-        print!("FPS: {}\n\x1b[1A", 1.0E+9 / last.to(now).num_nanoseconds().unwrap() as f64);
+        let delta = last.to(now).num_nanoseconds().unwrap() as f64 / 1.0E+9;
+        print!("FPS: {}\n\x1b[1A", 1.0/delta);
         last = now;
+
+        nemo.update(delta);
+
+
+        //
+        // Render
+        //
+        let uniforms_ui = uniform! {
+            cursor: (cursor.0 as f32, cursor.1 as f32), // <- (f64, f64) doesn't implement AsUniformValue
+            matrix: matrix_ui.clone()
+        };
+
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
+        let mut target = nemo.draw(target, world.clone()); // ㅇㅅㅇ?
+        target.draw(&vb_ui, &ib_ui, &program_ui, &uniforms_ui, &Default::default()).unwrap();
+        let _ = target.finish();
     }
 }
--- client/src/nemo.rs
+++ client/src/nemo.rs
@@ -7,13 +7,20 @@ pub struct Nemo {
     vb: VertexBuffer<Vertex>,
     ib: NoIndices,
     program: Program,
-    pos: (f32, f32),
+    state: State,
 }
 
 #[derive(Clone, Copy)]
 struct Vertex { position: [f32; 2] }
 
 implement_vertex!(Vertex, position);
+
+enum State {
+    /// Nemo is stopped
+    Stopped { pos: (f64, f64) },
+    /// Nemo is moving (0 <= t < 1)
+    Moving { src: (f64, f64), dest: (f64, f64), t: f64 }
+}
 
 impl Nemo {
     pub fn new<F: Facade>(facade: &F) -> Self {
@@ -44,15 +51,41 @@ impl Nemo {
                     color = vec3(1.0, 0.82745, 0.14118);
                 }
             "#, None).unwrap(),
-            pos: (0.0, 0.0)
+            state: State::Stopped { pos: (0.0, 0.0) },
+        }
+    }
+
+    pub fn update(&mut self, elapsed: f64) {
+        let mut next = None;
+
+        match self.state {
+            State::Stopped { .. } => {}
+            State::Moving { dest, ref mut t, .. } => {
+                *t += elapsed / 0.5;
+                if 1.0 <= *t { next = Some(State::Stopped { pos: dest }); }
+            }
+        };
+
+        next.map(|next| {
+            self.state = next;
+        });
+    }
+
+    fn current_pos(&self) -> (f64, f64) {
+        match self.state {
+            State::Stopped { pos } => pos,
+            State::Moving { src, dest, t } => {
+                (src.0*(1.0 - t) + dest.0*t, src.1*(1.0 - t) + dest.1*t)
+            }
         }
     }
 
     pub fn draw(&self, mut target: Frame, world: Matrix) -> Frame {
         use glium::Surface;
 
+        let pos = self.current_pos();
         let uniforms = uniform! {
-            pos: self.pos,
+            pos: (pos.0 as f32, pos.1 as f32), // <- (f64, f64) doesn't implement AsUniformValue
             matrix: world
         };
 
@@ -60,7 +93,8 @@ impl Nemo {
         target
     }
 
-    pub fn go(&mut self, dest: (f32, f32)) {
-        self.pos = dest;
+    pub fn go(&mut self, dest: (f64, f64)) {
+        let pos = self.current_pos();
+        self.state = State::Moving { src: pos, dest: dest, t: 0.0 };
     }
 }
