e4c625f990f4f4abdaab590d22d0fbbcc2f4d794
Seulgi Kim <dev@seulgi.kim>
2015-07-13 19:23:19 +0900

    Implement Game.
    And implement GameManager with Manager.

---
+++ server/src/game.rs
@@ -0,0 +1,22 @@
+use user::User;
+
+use manager::*;
+
+#[derive(Debug, Clone)]
+pub struct Game {
+    id: Id,
+    host: User,
+    guest: Option<User>,
+}
+
+impl Item<User> for Game {
+    fn new(id: &Id, host: &User) -> Self {
+        Game {
+            id: *id,
+            host: host.clone(),
+            guest: None,
+        }
+    }
+}
+
+pub type GameManager = Manager<Game, User>;
