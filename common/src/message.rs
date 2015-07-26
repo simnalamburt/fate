5c960a8f73ce01742fb9dbbf6424e67031c87ec2
Seulgi Kim <dev@seulgi.kim>
2015-07-26 14:04:56 +0900

    Add CreateGame message.

--- common/src/message.rs
+++ common/src/message.rs
@@ -6,11 +6,13 @@ use rustc_serialize::json::{DecodeResult, EncoderError};
 #[derive(RustcDecodable, RustcEncodable, PartialEq, Debug)]
 pub enum ServerToClient {
     ConnectResponse(usize), // User ID
+    CreateGameResponse(usize), // Game Id
 }
 
 #[derive(RustcDecodable, RustcEncodable, PartialEq, Debug)]
 pub enum ClientToServer {
     ConnectRequest,
+    CreateGameRequest(usize), // User Id
 }
 
 pub trait Message {
@@ -49,5 +51,21 @@ fn test_connect_request() {
     let original = ClientToServer::ConnectRequest;
     let encoded = original.stringify().unwrap();
     let parsed: ClientToServer = Message::parse(&encoded).unwrap();
+    assert_eq!(parsed, original);
+}
+
+#[test]
+fn test_create_game_request() {
+    let original = ClientToServer::CreateGameRequest(3);
+    let encoded = original.stringify().unwrap();
+    let parsed: ClientToServer = Message::parse(&encoded).unwrap();
+    assert_eq!(parsed, original);
+}
+
+#[test]
+fn test_create_game_response() {
+    let original = ServerToClient::CreateGameResponse(3);
+    let encoded = original.stringify().unwrap();
+    let parsed: ServerToClient = Message::parse(&encoded).unwrap();
     assert_eq!(parsed, original);
 }
