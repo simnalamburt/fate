6bb443374c0df313432ed8b9d5cd66ddc8e37ca7
Hyeon Kim <simnalamburt@gmail.com>
2015-08-09 17:17:38 +0900

    Let CreationError be able to be converted from io::Error

--- client/src/error.rs
+++ client/src/error.rs
@@ -1,10 +1,18 @@
+use std::io;
 use glium::vertex::BufferCreationError;
 use glium::program::ProgramCreationError;
 
 #[derive(Debug)]
 pub enum CreationError {
+    IoError(io::Error),
     BufferCreationError(BufferCreationError),
     ProgramCreationError(ProgramCreationError),
+}
+
+impl From<io::Error> for CreationError {
+    fn from(err: io::Error) -> CreationError {
+        CreationError::IoError(err)
+    }
 }
 
 impl From<BufferCreationError> for CreationError {
