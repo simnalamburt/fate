1d5eb90337c810e13f7ac9c1c97671ba6f4af235
Hyeon Kim <simnalamburt@gmail.com>
2015-08-10 10:34:40 +0900

    Add comments about resource locating
    Also, open issue #37

--- client/src/resource.rs
+++ client/src/resource.rs
@@ -4,6 +4,11 @@ use std::env::current_exe;
 use std::fs::File;
 
 pub fn load<T: AsRef<Path>>(name: T) -> Result<File> {
+    // During development, resources are located in `./client/res/*` and
+    // the binary is located in `./client/target/{debug, release}/client.exe`.
+    //
+    // `(file path of binary)/../../../res/(name)` will return desired
+    // resource's path
     let mut path = current_exe().unwrap();
     path.pop();
     path.pop();
