4f34f09161a66e38b1b53c482c20dbc847dcdb4e
Seulgi Kim <dev@seulgi.kim>
2015-08-17 16:25:31 +0900

    Implement a method to fill object id.

--- client/src/units/mod.rs
+++ client/src/units/mod.rs
@@ -5,6 +5,7 @@ use error::CreationError;
 use glium::{VertexBuffer, IndexBuffer, Program, Frame, DrawError, Surface};
 use glium::backend::Facade;
 use glium::draw_parameters::DrawParameters;
+use glium::framebuffer::SimpleFrameBuffer;
 use glium::uniforms::{AsUniformValue, Uniforms, UniformsStorage};
 use std::sync::atomic::{ATOMIC_USIZE_INIT, AtomicUsize, Ordering};
 use xmath::Matrix;
@@ -20,6 +21,7 @@ struct Unit {
     vb: VertexBuffer<Vertex>,
     ib: IndexBuffer<u16>,
     program: Program,
+    fill_id_program: Program,
     pos: Position,
     angle: f32,
 }
@@ -47,6 +49,19 @@ impl Unit {
             vb: vertex_buffer,
             ib: index_buffer,
             program: try!(Program::from_source(facade, vertex_shader, fragment_shader, None)),
+            fill_id_program: try!(Program::from_source(facade, r#"
+            #version 410
+            uniform mat4 matrix;
+            in vec3 position;
+            void main() {
+                gl_Position = matrix * vec4(position, 1.0);
+            }"#, r#"
+            #version 410
+            uniform vec4 id;
+            out vec4 color;
+            void main() {
+                color = id;
+            }"#, None)),
             pos: position,
             angle: 0.0,
         })
@@ -71,6 +86,15 @@ impl Unit {
         // TODO: Cache
         let uniforms = uniform! { matrix: matrix(self, camera) };
         draw_internal(target, &self, &self.program, &uniforms)
+    }
+
+    fn fill(&self, target: &mut SimpleFrameBuffer, camera: &Matrix) -> Result<(), DrawError> {
+        let red = ((self.id >> 24) & 0xFF) as f32 / 255.0;
+        let green = ((self.id >> 16) & 0xFF) as f32 / 255.0;
+        let blue = ((self.id >> 8) & 0xFF) as f32 / 255.0;
+        let alpha = (self.id & 0xFF) as f32 / 255.0;
+        let uniforms = uniform! { matrix: matrix(self, camera), id: [red, green, blue, alpha] };
+        draw_internal(target, &self, &self.fill_id_program, &uniforms)
     }
 }
 
