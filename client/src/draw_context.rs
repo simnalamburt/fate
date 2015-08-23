6027c0bc5533f0f2cff7c9f1e124e8352213c27d
Seulgi Kim <dev@seulgi.kim>
2015-08-23 17:49:43 +0900

    Move texture on DrawContext.

--- client/src/draw_context.rs
+++ client/src/draw_context.rs
@@ -1,13 +1,19 @@
+use glium::backend::Facade;
+use glium::texture::MipmapsOption::NoMipmap;
+use glium::texture::Texture2d;
+use glium::texture::UncompressedFloatFormat::U8U8U8U8;
 use xmath::Matrix;
 
 pub struct DrawContext {
     pub camera: Matrix,
+    pub texture_for_object_picking: Texture2d,
 }
 
 impl DrawContext {
-    pub fn new(width: f32, height: f32) -> DrawContext {
+    pub fn new<F>(display: &F, width: u32, height: u32) -> DrawContext where F: Facade {
         DrawContext {
-            camera: Matrix::orthographic(width/10.0, height/10.0, 0.0, 1.0),
+            camera: Matrix::orthographic(width as f32/10.0, height as f32/10.0, 0.0, 1.0),
+            texture_for_object_picking: Texture2d::empty_with_format(display, U8U8U8U8, NoMipmap, width, height).unwrap(),
         }
     }
 }
