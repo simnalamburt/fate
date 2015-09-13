12a567f8039ee0921fe20addf61750a5f14ad45c
Hyeon Kim <simnalamburt@gmail.com>
2015-09-14 03:23:23 +0900

    Perform resource packing only when build result doesn't exist
    Closes #45

--- client/build.rs
+++ client/build.rs
@@ -3,7 +3,7 @@ extern crate rustc_serialize;
 extern crate bincode;
 
 use std::env;
-use std::fs::File;
+use std::fs::{metadata, File};
 use std::io::BufReader;
 use std::path::Path;
 use std::env::current_exe;
@@ -12,6 +12,20 @@ use bincode::rustc_serialize as bcode;
 use bincode::SizeLimit;
 
 fn main() {
+    // Check if the rilakkuma has already packed
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest = Path::new(&out_dir).join("rilakkuma.obj.bin");
+
+    let meta = metadata(&dest);
+    let exists = if let Ok(meta) = meta {
+        meta.is_file()
+    } else {
+        false
+    };
+
+    // Perform resource packing only when rilakkuma.obj.bin doesn't exist
+    if exists { return; }
+
     // As of rust 1.2, `current_exe()` of build script looks like below:
     //
     //     /fate/client/target/debug/build/client-057d32d9862c7834/build-script-build
@@ -31,8 +45,6 @@ fn main() {
     let rilakkuma: Obj = load_obj(input).unwrap();
 
     // Serialize rilakkuma
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest = Path::new(&out_dir).join("rilakkuma.obj.bin");
     let mut output = File::create(&dest).unwrap();
 
     bcode::encode_into(&rilakkuma, &mut output, SizeLimit::Infinite).unwrap();
